5646636ffe666e3858b6c2264524058c1db7d50d
Jo <johj703@nate.com>
2024-08-28 12:02:50 +0900

    PokemonList 컴포넌트 작성

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,5 +1,18 @@
+import PokemonCard from "./PokemonCard";
+
 const PokemonList = () => {
-  return <div>PokemonList</div>;
+  return (
+    <div>
+      {PokemonList.map((pokemon) => {
+        <PokemonCard
+          key={pokemon.id}
+          pokemon={{}}
+          onAdd={() => {}}
+          isSelected={false}
+        />;
+      })}
+    </div>
+  );
 };
 
 export default PokemonList;

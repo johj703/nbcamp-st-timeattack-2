1c8171d067c8568ee8f8db61f02df1e685fda777
Jo <johj703@nate.com>
2024-08-28 12:03:04 +0900

    PokemonCard 컴포넌트 작성

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,12 @@
+const PokemonCard = () => {
+  return (
+    <div>
+      <img src={""} alt={""} />
+      <p>{PokemonCard.korean_name}</p>
+      <button>삭제</button>
+      <button>추가</button>
+    </div>
+  );
+};
+
+export default PokemonCard;

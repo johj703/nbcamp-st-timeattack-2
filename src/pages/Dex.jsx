ed511f86d2fffa56d7a1fcb91c6b1c6d06c82c30
Jo <johj703@nate.com>
2024-08-28 12:02:27 +0900

    Dex 페이지에 Dashboard, PokemonList 컴포넌트 추가

--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -1,5 +1,16 @@
+import Dashboard from "../components/Dashboard";
+import PokemonList from "../components/PokemonList";
+
 const Dex = () => {
-  return <div>Dex</div>;
+  return (
+    <div>
+      <h1>포켓몬 도감</h1>
+      <h3>선택된 포켓몬들이 나타납니다</h3>
+      <Dashboard />
+      <hr />
+      <PokemonList />
+    </div>
+  );
 };
 
 export default Dex;

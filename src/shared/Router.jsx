f6a55051998693408486a18ff5a6b5fac030ffc4
Jo <johj703@nate.com>
2024-08-28 11:38:29 +0900

    포켓몬 도감 대문 페이지 작성 및 Router에 등록해서 화면에 나타내도록 작성

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,9 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </BrowserRouter>
   );
 };

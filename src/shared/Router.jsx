242dc357a021a607c1ca7d495d354816c7a1cd95
Jo <johj703@nate.com>
2024-08-28 12:01:22 +0900

    Router에 dex페이지를 Route에 추가

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
+import Dex from "../pages/Dex";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/dex" element={<Dex />} />
       </Routes>
     </BrowserRouter>
   );

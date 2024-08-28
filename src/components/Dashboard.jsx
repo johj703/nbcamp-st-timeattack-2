22332a9afe2738e113b7d88427143fea83cb944e
Jo <johj703@nate.com>
2024-08-28 11:45:50 +0900

    Dashboard 컴포넌트 초기화

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,5 +1,18 @@
-const Dashboard = () => {
-  return <div>Dashboard</div>;
+const Dashboard = ({ selectedPokemon }) => {
+  return (
+    <div>
+      <h2>대시보드</h2>
+      {selectedPokemon.length === 0 ? (
+        <p>선택된 포켓몬이 없습니다.</p>
+      ) : (
+        <ul>
+          {selectedPokemon.map((pokemon) => {
+            <li key={pokemon.id}>{pokemon.korean_name}</li>;
+          })}
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default Dashboard;
